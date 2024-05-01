56c34b3fd7c284436ad6563f11a436c8d80aaefa
Arsath mohamed <165041940+arsath-02@users.noreply.github.com>
2024-05-01 11:17:22 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors:{
+        primary: '#29AB30',
+        secondary:'#0E87CC',
+        lb:'#89CFF0',
+      },
+    },
+  },
+  plugins: [],
+};
